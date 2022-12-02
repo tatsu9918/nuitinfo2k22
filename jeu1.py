a16dc7d60729dcb86114af90939637434ec54219
Yeetnus <yaniscroguennoc@gmail.com>
2022-12-02 02:05:00 +0100

    Pense avoir fini

---
+++ jeu1.py
@@ -0,0 +1,349 @@
+from random import *
+mot="Chlamydia"
+print(mot)
+nbessai=6
+essaie=1
+text=input
+def afficher_mot(mot):
+    print(mot)
+
+def bon(mot,el,text):
+    if text[el]==mot[el]:
+        text[el]=bon
+        return True
+    return False
+
+def dans(mot,el,text):
+    if text[el] in li[a]:
+        text[el]=dans
+        return True
+    return False
+
+def ecrire(text,el,ide):
+    msg = document.getElementById("ide")
+    msg.innerHTML = text[el]
+
+while essaie<=nbessai:
+    for i in len(text)
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c1l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c1l1").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c2l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c2l1").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c3l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c3l1").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c4l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c4l1").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c5l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c5l1").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c6l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c6l1").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c7l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c7l1").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c8l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c8l1").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c9l1").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c9l1").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c1l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c1l2").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c2l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c2l2").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c3l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c3l2").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c4l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c4l2").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c5l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c5l2").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c6l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c6l2").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c7l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c7l2").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c8l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c8l2").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c9l2").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c9l2").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c1l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c1l3").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c2l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c2l3").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c3l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c3l3").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c4l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c4l3").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c5l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c5l3").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c6l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c6l3").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c7l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c7l3").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c8l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c8l3").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c9l3").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c9l3").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c1l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c1l4").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c2l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c2l4").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c3l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c3l4").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c4l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c4l4").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c5l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c5l4").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c6l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c6l4").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c7l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c7l4").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c8l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c8l4").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c9l4").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c9l4").style.color = "yellow"
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c1l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c1l5").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c2l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c2l5").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c3l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c3l5").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c4l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c4l5").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c5l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c5l5").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c6l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c6l5").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c7l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c7l5").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c8l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c8l5").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c9l5").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c9l5").style.color = "yellow"
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c1l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c1l6").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c2l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c2l6").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c3l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c3l6").style.color = "yellow"
+
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c4l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c4l6").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c5l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c5l6").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c6l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c6l6").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c7l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c7l6").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c8l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c8l6").style.color = "yellow"
+        
+        if bon(mot,i,text)==True:
+            ecrire(text,i,c1)
+            document.getElementById("c9l6").style.color= "red"
+        if dans(mot,i,text)==True:
+            document.getElementById("c9l6").style.color = "yellow"
+        
